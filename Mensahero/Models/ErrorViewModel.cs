c8ce14cab64549ba8bb6098b926336b7efb55a9e
Kent Oroceo <kjoroceo@yahoo.com>
2021-06-15 20:20:03 +0800

    Add project files.

---
+++ Mensahero/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mensahero.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
